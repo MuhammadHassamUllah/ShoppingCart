fd2d4884062d077773b2d2b4bdcaffc3d27efb7f
Muhammad Hassam <hassamshoaib96@gmail.com>
2018-02-06 08:51:45 -0800

    Add cart functionality using connect-mongo

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 var User = require("../models/user");
 var Product = require("../models/product");
+var Cart  = require("../models/cart");
 
 /* GET home page. */
 router.get('/', function (req, res) {
@@ -18,11 +19,24 @@ router.get('/', function (req, res) {
                 //prodConcatList.push(split())
                 prodConcatList.push(prodList.slice(i, i+3));
             }
-            console.log(prodConcatList);
             res.render('shop/index', {products: prodConcatList});
         }
     });
 });
 
+router.get('/add-to-cart/:id', function(req, res){
+    console.log("Add to cart route accesssed\n\n\n");
+    var productId = req.params.id;
+    var cart = new Cart(req.session.cart ? req.session.cart : {});
 
+    Product.findById(productId, function(err, product){
+        if(err){
+            res.send("product not found");
+        }
+        cart.add(product, productId);
+        req.session.cart = cart;
+        console.log(req.session.cart);
+        res.redirect('/');
+    })
+});
 module.exports = router;
